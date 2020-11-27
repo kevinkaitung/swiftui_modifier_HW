5dc8f4ecfa76b9d141fd2f3cedd34a6a131de6a4
as468579 <as468579@gmail.com>
2020-11-27 16:07:01 +0800

    Initial Commit

---
+++ swiftui_modifier_HW/swiftui_modifier_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_modifier_HWApp.swift
+//  swiftui_modifier_HW
+//
+//  Created by User18 on 2020/11/27.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_modifier_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
